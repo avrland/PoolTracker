e43f622bad6cb62c85288cfb77fe17dfda2212fd
Marcin Popko <marcinpopko@outlook.com>
2023-11-01 17:04:36 +0100

    Day saving time fixing

--- scrapper.py
+++ scrapper.py
@@ -32,13 +32,18 @@ def single_api_request():
 
 def push_to_db():
     current_time = datetime.now()
-    one_hour = timedelta(hours=2)
+    localtime = time.localtime()
+    summer_time = localtime.tm_isdst
+    if summer_time:
+        one_hour = timedelta(hours=2)
+    if summer_time == 0:
+        one_hour = timedelta(hours=1)
     new_time = current_time + one_hour
     single_scrap_content = single_api_request()
     if single_scrap_content is None:
         print("Error receiving data from API.")
         return
-    print(str(current_time), str(single_scrap_content))
+    print(str(new_time), str(single_scrap_content))
     sport, family, small, ice = single_scrap_content['sport'], single_scrap_content['family'], single_scrap_content['small'], single_scrap_content['ice']
     insert_stats(new_time, people_sport=sport, people_family=family, people_small=small, people_ice=ice)
 
