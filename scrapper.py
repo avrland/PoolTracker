928a002bf9e0b6ef5fb1ef07caff93df65d45e83
Marcin Popko <marcinpopko@outlook.com>
2024-03-31 08:29:59 +0200

    DTZ changes detection

--- scrapper.py
+++ scrapper.py
@@ -4,6 +4,7 @@ from db_handler import insert_stats
 from datetime import datetime, timedelta
 import schedule
 import time
+import pytz 
 
 def single_api_request():
     try:
@@ -31,14 +32,8 @@ def single_api_request():
     return single_scrap
 
 def push_to_db():
-    current_time = datetime.now()
-    localtime = time.localtime()
-    summer_time = localtime.tm_isdst
-    if summer_time:
-        one_hour = timedelta(hours=2)
-    if summer_time == 0:
-        one_hour = timedelta(hours=1)
-    new_time = current_time + one_hour
+    pl = pytz.timezone('Europe/Warsaw')
+    new_time = datetime.now().astimezone(pl)
     single_scrap_content = single_api_request()
     if single_scrap_content is None:
         print("Error receiving data from API.")
@@ -46,8 +41,7 @@ def push_to_db():
     print(str(new_time), str(single_scrap_content))
     sport, family, small, ice = single_scrap_content['sport'], single_scrap_content['family'], single_scrap_content['small'], single_scrap_content['ice']
     insert_stats(new_time, people_sport=sport, people_family=family, people_small=small, people_ice=ice)
-
-
+    
 schedule.every().hour.at(":00").do(push_to_db)
 schedule.every().hour.at(":15").do(push_to_db)
 schedule.every().hour.at(":30").do(push_to_db)
